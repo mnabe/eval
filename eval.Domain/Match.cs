5c899b98ccddd17caa2bbc5a1c662bf36205313f
mnabe <32497085+mnabe@users.noreply.github.com>
2021-02-05 22:29:37 +0100

    Add domain project

---
+++ eval.Domain/Match.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eval.Domain
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Opponent { get; set; }
+        public string ReasonForLoss { get; set; }
+    }
+}
