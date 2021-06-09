ad5f5a741fc4c8a02a82be822b26114bb36ad837
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-09 20:43:40 +0200

    Whitespace

--- src/eval.Web/Areas/Identity/Data/ApplicationUser.cs
+++ src/eval.Web/Areas/Identity/Data/ApplicationUser.cs
@@ -9,5 +9,6 @@ namespace eval.Web.Areas.Identity.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
-    {}
+    {
+    }
 }
