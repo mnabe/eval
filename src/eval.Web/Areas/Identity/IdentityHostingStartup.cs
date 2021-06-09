b289d4f5342d46106a5965e8b5c2ae1e3a41d531
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-09 21:07:37 +0200

    Free users from having to confirm account before it being usable

--- src/eval.Web/Areas/Identity/IdentityHostingStartup.cs
+++ src/eval.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -20,7 +20,7 @@ namespace eval.Web.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("AuthDbContextConnection")));
 
-                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                     .AddEntityFrameworkStores<AuthDbContext>();
             });
         }
