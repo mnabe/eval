035219cdd131caa3985371ff9f98e3db8c8aa958
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-09 20:35:12 +0200

    Add property for username

--- src/eval.Web/Areas/Identity/Data/ApplicationUser.cs
+++ src/eval.Web/Areas/Identity/Data/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,7 @@ namespace eval.Web.Areas.Identity.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Column(TypeName = "nvarchar(40)")]
+        public string Username { get; set; }
     }
 }
