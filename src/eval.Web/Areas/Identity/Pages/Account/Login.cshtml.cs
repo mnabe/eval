e597472f02a22f27f9c8bda5c0275c9a283147a0
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-24 14:45:24 +0200

    Temporarily redirect to privacy page upon login

--- src/eval.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/eval.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -88,7 +88,7 @@ namespace eval.Web.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return LocalRedirect("~/Home/Privacy");
                 }
                 if (result.RequiresTwoFactor)
                 {
