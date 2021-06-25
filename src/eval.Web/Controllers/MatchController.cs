7d1789e5379a08cf70e870a36f0e35bc07ca422a
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:10:19 +0200

    Add HttpRequests from MVC Controller

--- src/eval.Web/Controllers/MatchController.cs
+++ src/eval.Web/Controllers/MatchController.cs
@@ -1,18 +1,44 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using eval.Web.ApiConfig;
+using eval.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text;
 
 namespace eval.Web.Controllers
 {
     [Authorize]
     public class MatchController : Controller
     {
-        public IActionResult Index()
+        public MatchController()
+        {
+            ApiHelper.InitializeClient();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response = await ApiHelper.ApiClient.GetAsync("");
+            var result = await response.Content.ReadAsStringAsync();
+            List<MatchViewModel> match = JsonSerializer.Deserialize<List<MatchViewModel>>(result);
+            return View(match);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CreateMatch(CreateMatchViewModel model)
+        {
+            var json = JsonSerializer.Serialize(model);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await ApiHelper.ApiClient.PostAsync("", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return Ok(responseString);
         }
     }
 }
