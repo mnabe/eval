687c12921fcdecfe9e001f8cd01a7ee3d8a81e49
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:09:20 +0200

    Add DbContext

---
+++ src/eval.Persistence/TempContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace eval.Persistence
+{
+    public class TempContext: DbContext
+    {
+        public TempContext(DbContextOptions<TempContext> options): base(options)
+        {
+        }
+
+        public virtual DbSet<MatchEntity> MatchEntities { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<MatchEntity>()
+                .HasKey(c => c.Id);
+        }
+    }
+}
