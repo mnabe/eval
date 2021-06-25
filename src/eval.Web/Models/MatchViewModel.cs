f6249ba6d1a5792e40889d72d7c6bcad45f19d7e
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:15:31 +0200

    Add Id to MatchViewModel

--- src/eval.Web/Models/MatchViewModel.cs
+++ src/eval.Web/Models/MatchViewModel.cs
@@ -7,6 +7,7 @@ namespace eval.Web.Models
 {
     public class MatchViewModel
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public string OpponentName { get; set; }
         public string ReasonForLoss { get; set; }
