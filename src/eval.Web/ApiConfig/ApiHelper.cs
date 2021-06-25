dbd50cc0b86e4dd547ee0ee19ff60520299ac8b5
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:09:52 +0200

    ApiHelper-class for HttpClient in MVC Controller

---
+++ src/eval.Web/ApiConfig/ApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace eval.Web.ApiConfig
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://localhost:44393/api/Match/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
