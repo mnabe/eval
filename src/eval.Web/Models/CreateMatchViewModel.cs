22938cf27f70106a6ed6f7a350786153e9c2b020
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:15:21 +0200

    Make ViewModel specifically for creating matches

---
+++ src/eval.Web/Models/CreateMatchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eval.Web.Models
+{
+    public class CreateMatchViewModel
+    {
+        public DateTime Date { get; set; }
+        public string OpponentName { get; set; }
+        public string ReasonForLoss { get; set; }
+    }
+}
