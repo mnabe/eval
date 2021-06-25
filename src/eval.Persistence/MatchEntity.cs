48eb27d829773fae31f0a32c46b7d1c6ecde4078
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:09:34 +0200

    Add a db entity for match

---
+++ src/eval.Persistence/MatchEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eval.Persistence
+{
+    public class MatchEntity
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string OpponentName { get; set; }
+        public string ReasonForLoss { get; set; }
+    }
+}
