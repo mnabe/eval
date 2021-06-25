7532110c6e20a25ed402c63c9ffd8fdd15831b01
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:09:26 +0200

    Add repository

---
+++ src/eval.Persistence/Repository.cs
@@ -0,0 +1,45 @@
+﻿using eval.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eval.Persistence
+{
+    public class Repository
+    {
+        private readonly TempContext _context;
+
+        public Repository(TempContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Match> GetAll()
+        {
+            var response = _context.MatchEntities.AsEnumerable();
+            List<Match> matches = new List<Match>();
+            foreach (var item in response)
+            {
+                Match match = new Match();
+                match.Id = item.Id;
+                match.Date = item.Date;
+                match.OpponentName = item.OpponentName;
+                match.ReasonForLoss = item.ReasonForLoss;
+                matches.Add(match);
+            } 
+            return matches;
+        }
+
+        public void Create(Match match)
+        {
+            MatchEntity entity = new MatchEntity();
+            entity.Date = match.Date;
+            entity.OpponentName = match.OpponentName;
+            entity.ReasonForLoss = match.ReasonForLoss;
+            _context.MatchEntities.Add(entity);
+            _context.SaveChanges();
+        }
+    }
+}
