43b5c8a29f561ecf37ed93552236fe55397c47f9
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-25 14:06:08 +0200

    Create DTO for sending POST request

---
+++ src/eval.API/DTO/CreateMatchDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eval.API.DTO
+{
+    public class CreateMatchDto
+    {
+        public DateTime Date { get; set; }
+        public string OpponentName { get; set; }
+        public string ReasonForLoss { get; set; }
+    }
+}
