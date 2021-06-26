c8e57e244a2408c5e69619e25afd46de8dfcdaaf
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-26 14:12:40 +0200

    Add username to dto

--- src/eval.API/DTO/CreateMatchDto.cs
+++ src/eval.API/DTO/CreateMatchDto.cs
@@ -7,6 +7,7 @@ namespace eval.API.DTO
 {
     public class CreateMatchDto
     {
+        public string UserName { get; set; }
         public DateTime Date { get; set; }
         public string OpponentName { get; set; }
         public string ReasonForLoss { get; set; }
