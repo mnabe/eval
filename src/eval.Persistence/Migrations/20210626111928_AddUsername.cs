fb1cfa7cc2274f0c06e5f5ae383e272f790e6c62
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-26 14:10:37 +0200

    Migration to add username to db table

---
+++ src/eval.Persistence/Migrations/20210626111928_AddUsername.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eval.Persistence.Migrations
+{
+    public partial class AddUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "MatchEntities",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "MatchEntities");
+        }
+    }
+}
