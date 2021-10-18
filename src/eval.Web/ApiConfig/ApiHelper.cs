ff4d2b3ef60c79512ae48104aa7fadbf59ccf325
mnabe <mathiasnabenielsen@gmail.com>
2021-10-18 18:25:50 +0200

    Access IConfiguration/.json-appSettings files from static class

--- src/eval.Web/ApiConfig/ApiHelper.cs
+++ src/eval.Web/ApiConfig/ApiHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
@@ -7,12 +9,23 @@ namespace eval.Web.ApiConfig
     public static class ApiHelper
     {
         public static HttpClient ApiClient { get; set; }
+        public static IConfiguration Config { get; set; }
         public static void InitializeClient()
         {
+            Config = ConfigurationManager();
+            string ApiURL = Config.GetValue<string>("ApiURL");
             ApiClient = new HttpClient();
-            ApiClient.BaseAddress = new Uri("https://evalwebmvc.azurewebsites.net/matchindex/api/Match"); //https://localhost:44393/api/Match/ //https://evalwebmvc.azurewebsites.net/matchindex
+            ApiClient.BaseAddress = new Uri(ApiURL); //https://localhost:44393/api/Match/
             ApiClient.DefaultRequestHeaders.Accept.Clear();
             ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        static IConfiguration ConfigurationManager()
+        {
+            Config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appSecrets.json")
+                    .Build();
+            return Config;
         }
     }
 }
