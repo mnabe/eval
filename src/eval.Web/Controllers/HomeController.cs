9f1fc7162c4bd69ba9615e9506533aa2bdaef236
mnabe <mathiasnabenielsen@gmail.com>
2022-01-22 13:33:49 +0100

    Add changelog

--- src/eval.Web/Controllers/HomeController.cs
+++ src/eval.Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace eval.Web.Controllers
             return View();
         }
 
+        public IActionResult Changelog()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
